64da54fe18b94557e49ced00a53f334dc5c1fc29
Moshe Rasetsoke <marulacoder@gmail.com>
2023-08-24 11:58:44 +0200

    checkpoint - result class implemented

--- TaxCalculator.Domain/Shared/Result.cs
+++ TaxCalculator.Domain/Shared/Result.cs
@@ -8,5 +8,35 @@ namespace TaxCalculator.Domain.Shared
 {
     public class Result
     {
+        #region Constructors
+
+        private Result(bool isSuccessful, string errorMessage) 
+        {
+            if (!isSuccessful && string.IsNullOrWhiteSpace(errorMessage))
+            { 
+                throw new InvalidOperationException("Error message cannot be an empty value.");
+            }
+
+            IsSuccessful = isSuccessful;
+            ErrorMessage = errorMessage;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsSuccessful { get; }
+        public bool IsFailure => !IsSuccessful;
+        public string ErrorMessage { get; }
+
+        #endregion
+
+        #region Public Methods
+
+        public static Result Success() => new Result(true, string.Empty);
+
+        public static Result Failure(string errorMessage) => new Result(false, errorMessage);
+
+        #endregion
     }
 }
